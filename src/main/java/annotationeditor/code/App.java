0dba7d329b36094147d3b4ba1250a771db4e50b9
jason <jasonjasonyehyeh@gmail.com>
2022-04-14 16:49:28 +0800

    Fixed LoaderProblem

--- src/main/java/annotationeditor/code/App.java
+++ src/main/java/annotationeditor/code/App.java
@@ -11,15 +11,16 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("AppLayout.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        System.out.println(fxmlLoader);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("註解編輯器");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args)
     {
+        launch();
         System.out.println("321321321");
-
     }
 }
