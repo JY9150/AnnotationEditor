35555f306064ac073e5a8fc1c1bb3e6bb00d4e5b
howard <howard50515@gmail.com>
2022-04-15 13:46:16 +0800

    讀寫txt文件,codeType功能完善

--- src/main/java/annotationeditor/code/FileSystem/File_System.java
+++ src/main/java/annotationeditor/code/FileSystem/File_System.java
@@ -3,6 +3,7 @@ package annotationeditor.code.FileSystem;
 import com.google.gson.Gson;
 import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class File_System {
@@ -37,4 +38,39 @@ public class File_System {
         }
     }
 
+    public static void saveAsTxt(String saveObject, String path){
+        try{
+            PrintWriter file = new PrintWriter(path);
+            file.write(saveObject);
+            file.flush();
+            file.close();
+        }
+        catch (IOException e){
+            System.out.println("File_System : save file to "+ path +" fail , please check the folder");
+        }
+    }
+    public static void saveAsTxt(ArrayList<String> saveObjectList, String path){saveAsTxt(saveObjectList.toArray(new String[0]),path);}
+    public static void saveAsTxt(String[] saveObjectArray, String path){
+        String temp = "";
+        for (int i=0;i < saveObjectArray.length;i++) {
+            temp+=saveObjectArray[i] ;
+            if(i < saveObjectArray.length-1) temp+='\n';
+        }
+        saveAsTxt(temp,path);
+    }
+
+
+    public static ArrayList<String> loadFromTxt(String path){
+        ArrayList<String> dataArrayList = new ArrayList<>();
+        try{
+            Scanner scanner = new Scanner(new File(path));
+            while (scanner.hasNext()){
+                dataArrayList.add(scanner.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("File_System : load file from "+ path +" fail");
+        }
+        return dataArrayList;
+    }
 }
