f09d6722c1d7a264c01409d13e3b2778ec3af811
jason <jasonjasonyehyeh@gmail.com>
2022-04-16 21:51:38 +0800

    + 註解 + UI按鈕

--- src/main/java/annotationeditor/code/AppController.java
+++ src/main/java/annotationeditor/code/AppController.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.File;
 import java.net.URL;
@@ -13,7 +15,7 @@ import java.util.ResourceBundle;
 public class AppController implements Initializable {
     // this is Jason commit test
     @FXML
-    private TreeView<String> treeTable;
+    private TreeView<String> fileView_treeTable;
     @FXML
     private TextField RootPath_textField;
     @FXML
@@ -21,28 +23,53 @@ public class AppController implements Initializable {
     @FXML
     private ComboBox<String> fileFilter_comboBox;
     private final ObservableList<String> ob = FXCollections.observableArrayList("All", ".java", ".cs", ".txt");
+    @FXML
+    private ComboBox<String> codeTypeFilter_comboBox;
+    private ObservableList<String> ob2 = FXCollections.observableArrayList("codeType1","codeType2");
+    @FXML
+    private VBox DDD;
 
 
     @Override
+    /** Initializing UI
+     * */
     public void initialize(URL url, ResourceBundle rb){
         fileFilter_comboBox.setItems(ob);
         fileFilter_comboBox.setValue("All");
+        codeTypeFilter_comboBox.setItems(ob2);
+        codeTypeFilter_comboBox.setValue("codeType1");
+        loadTreeTable();
+        //DDD.setVisible(false);
+    }
+    /**若有更改 rootPath,fileFilter 重設 fileView_treeTable
+     *
+     * */
+    public void reloadTreeTable(){
         loadTreeTable();
     }
-
-    public void Changed(){
-        loadTreeTable();
-    }
-
+    /** fileView_treeTable 物件被選擇
+     *
+     * */
     public void selectItem(){
-        //???
-        TreeItem<String> item = treeTable.getSelectionModel().getSelectedItem();
+        TreeItem<String> item = fileView_treeTable.getSelectionModel().getSelectedItem();
         if (item != null){
             System.out.println(item.getValue());
         }
     }
+    /** fileView_treeTable 物件被連點
+     * */
+    public void doubleClickSelectItem(){
 
+    }
+    /** 返回建觸發
+     * */
+    // TODO: 2022/4/16
+    public void backButtonOnClick(){
+        System.out.println("Back");
+    }
 
+    /* 重整fileView_treeTable
+    * */
     private void loadTreeTable(){
         String inpath = RootPath_textField.getText();
         File rootFile = new File(inpath);
@@ -50,12 +77,12 @@ public class AppController implements Initializable {
             TreeItem<String> root = new TreeItem<>(rootFile.getName());
             findInner(rootFile, root, fileFilter_comboBox.getValue() != "All" );
             root.setExpanded(true);
-            treeTable.setRoot(root);
+            fileView_treeTable.setRoot(root);
         } else {
-            treeTable.setRoot(new TreeItem<>("Invalidate Path"));
+            fileView_treeTable.setRoot(new TreeItem<>("Invalidate Path"));
         }
     }
-
+    @Deprecated
     private void findInner(File file, TreeItem<String> root){
         File[] innerfiles = file.listFiles();
         for (File value : innerfiles) {
@@ -72,7 +99,8 @@ public class AppController implements Initializable {
         }
 
     }
-
+    /*
+     */
     private void findInner(File file, TreeItem<String> root, boolean fileFilter){
         File[] innerfiles = file.listFiles();
         for (File value : innerfiles) {
@@ -95,15 +123,31 @@ public class AppController implements Initializable {
         }
 
     }
+    private void findInner2(File file, TreeItem<String> root, boolean fileFilter){
+        File[] innerfiles = file.listFiles();
+        for (File value : innerfiles) {
+            if (value.isDirectory()) {
+                TreeItem<String> innerRoot = new TreeItem<>(value.getName());
+
+                findInner(value, innerRoot,fileFilter);
+                root.getChildren().add(innerRoot);
+            }
+        }
+        for (File value : innerfiles) {
+            if (fileFilter){
+                if (value.isFile() && passFileFilter(value)) {
+                    root.getChildren().add((new TreeItem<>(value.getName())));
+                }
+            } else {
+                if (value.isFile()) {
+                    root.getChildren().add((new TreeItem<>(value.getName())));
+                }
+            }
+        }
+    }
+    /* Check if the File object is the file type we want
+    * */
     private boolean passFileFilter(File file){
         return file.getName().contains(fileFilter_comboBox.getValue());
-
-//        String[] filters = new String[4];
-//        for (String s : filters) {
-//            if (s.equals(file.getName())){
-//                return true;
-//            }
-//        }
-//        return false;
     }
 }
